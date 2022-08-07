54b13c917628cfd95b47acd44b4aa133729f6350
dmotrio <dmitri.ollari@protonmail.com>
2022-08-07 15:33:44 +0200

    test

--- nvim/lua/keymaps/init.lua
+++ nvim/lua/keymaps/init.lua
@@ -5,6 +5,8 @@ local keymap = vim.api.nvim_set_keymap
 keymap('n', '<c-s>', ':w<CR>', {})            -- ctrl + s to save doc
 keymap('i', '<c-s>', '<Esc>:w<CR>a', {})      -- ctrl + s in interactive mode to save and continue editing
 
+keymap('n', '<c-q>', ':q<CR>', {})            -- ctrl + s to save doc
+keymap('i', '<c-q>', '<Esc>:q<CR>a', {})      -- ctrl + s in interactive mode to save and continue editing
 
 -- keymaps for telescope plugin
 keymap('n', '<c-f>', ':Telescope find_files<CR>', {})
