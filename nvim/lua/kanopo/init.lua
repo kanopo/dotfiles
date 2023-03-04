1406682ca7f505c75e019a3f62a1991750a7716c
kanopo <dmitri.ollari@protonmail.com>
2023-03-04 12:24:17 +0100

    deleted old configs and initialize lazy

---
+++ nvim/lua/kanopo/init.lua
@@ -0,0 +1,2 @@
+
+require("kanopo.lazy")
---
+++ nvim/lua/kanopo/lazy.lua
@@ -0,0 +1,26 @@
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+plugins = {
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme gruvbox]])
+		end,
+	},
+}
+opts = {}
+require("lazy").setup(plugins, otps)
