70539e1f974dc5c265fc32066b12fbdf8f061aab
kanopo <dmitri.ollari@protonmail.com>
2023-03-04 15:57:00 +0100

    added tmux movement

--- nvim/lua/kanopo/keymaps.lua
+++ nvim/lua/kanopo/keymaps.lua
@@ -15,3 +15,7 @@ nmap("<leader>ff", ":Telescope find_files<CR>", "[F]ind [F]iles")
 nmap("<leader>ht", ":Telescope help_tags<CR>", "[H]elp [T]ags")
 nmap("<leader>gb", ":Telescope git_branches<CR>", "[G]it [B]ranches")
 
+nmap("C-h", ":TmuxNavigateLeft<CR>", "Move left in the split View")
+nmap("C-j", ":TmuxNavigateDown<CR>", "Move down in the split View")
+nmap("C-k", ":TmuxNavigateUp<CR>", "Move up in the split View")
+nmap("C-l", ":TmuxNavigateRight<CR>", "Move right in the split View")
