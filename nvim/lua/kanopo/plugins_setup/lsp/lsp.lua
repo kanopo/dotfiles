05cdf9453a370c77705b2d93a5275ad957656368
kanopo <dmitri.ollari@protonmail.com>
2023-03-04 16:08:49 +0100

    modifica lsp

--- nvim/lua/kanopo/plugins_setup/lsp/lsp.lua
+++ nvim/lua/kanopo/plugins_setup/lsp/lsp.lua
@@ -24,9 +24,59 @@ mason.setup({})
 mason_lspconfig.setup({
   ensure_installed = {
     "lua_ls",
+    "pyright",
   },
+  automatic_installation = true,
 })
 
-lspconfig.lua_ls.setup({})
+local on_attach = function(_, bufnr)
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = "LSP: " .. desc
+    end
+
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+  -- See `:help K` for why this keymap
+  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+  -- Lesser used LSP functionality
+  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+  nmap("<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, "[W]orkspace [L]ist Folders")
+
+  nmap("<leader>f", function()
+    vim.lsp.buf.format({ async = true })
+  end, "[F]ormat")
+end
+
+local lua_settings = require("kanopo.plugins_setup.lsp.lua_ls").settings
+
+lspconfig.lua_ls.setup({
+  on_attach = on_attach,
+  settings = lua_settings,
+})
+
+
+local python_settings = require("kanopo.plugins_setup.lsp.pyright").settings
+lspconfig.pyright.setup({
+  on_attach = on_attach,
+  settings = python_settings,
+})
 
 
