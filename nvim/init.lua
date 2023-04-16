c2eb90468275acd0b500aba169ef6fe30750decc
kanopo <dmitri.ollari@protonmail.com>
2023-04-16 12:05:36 +0200

    asd

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,3 @@ require("kanopo.options")
 require("kanopo.autocmds")
 require("kanopo.lazy")
 require("kanopo.keymaps")
-
-vim.cmd([[ autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
-
