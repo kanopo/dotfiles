a6915ebe2e1222513e37cb4e0e076317498e0dc5
kanopo <dmitri.ollari@protonmail.com>
2023-09-28 17:39:41 +0200

    edit tmux

--- nvim/lua/kanopo/plugins/indent_guide.lua
+++ nvim/lua/kanopo/plugins/indent_guide.lua
@@ -4,16 +4,22 @@ vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
+
 M = {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   opts = {
-    char = "┊",
-    show_trailing_blankline_indent = false,
-    enabled = true,
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
+  --   char = "┊",
+  --   show_trailing_blankline_indent = false,
+  --   enabled = true,
+  --   space_char_blankline = " ",
+  --   show_current_context = true,
+  --   show_current_context_start = true,
+  -- }
+
+
   }
+
 
 }
 
