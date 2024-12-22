cc01ac001b675831f645fa72d01d4b4f0af8fe9d
Dmitri <dmitri.ollari@protonmail.com>
2024-12-22 14:50:00 +0100

    added oil

--- nvim/lua/kanopo/plugins/neotree.lua
+++ nvim/lua/kanopo/plugins/neotree.lua
@@ -1,12 +1,13 @@
-return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    keys = {
-        { "<leader>fe", "<cmd>Neotree toggle<cr>", desc = "Toggle file explorer" },
-    }
-}
+return {}
+-- return {
+--     "nvim-neo-tree/neo-tree.nvim",
+--     branch = "v3.x",
+--     dependencies = {
+--         "nvim-lua/plenary.nvim",
+--         "nvim-tree/nvim-web-devicons",
+--         "MunifTanjim/nui.nvim",
+--     },
+--     keys = {
+--         { "<leader>fe", "<cmd>Neotree toggle<cr>", desc = "Toggle file explorer" },
+--     }
+-- }
